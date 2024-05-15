fe4d266e14de26b7a98587472f83212684e1f6a5
Ibenhoe <iben.vant@gmail.com>
2024-05-15 10:44:48 +0200

    Ehb logo to homepage
    Ehb logo to homepage fix
    
    Co-Authored-By: Andrei Preda <86624624+Andreipredaa11@users.noreply.github.com>

--- medialab3/resources/views/home/header.blade.php
+++ medialab3/resources/views/home/header.blade.php
@@ -8,7 +8,7 @@
             <div class="col-12">
                 <nav class="main-nav">
                     <!-- ***** Logo Start ***** -->
-                    <a href="index.html" class="logo">
+                    <a href="{{url('/')}}" class="logo">
                         <img src="assets/images/eraslogo-white.png" alt="">
                     </a>
                     <!-- ***** Logo End ***** -->
